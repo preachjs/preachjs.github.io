0cac812dadd63b10ccdabf173a11709eab9a1be3
reaper <ahoy@barelyhuman.dev>
2024-12-29 11:33:08 +0530

    fix: build crashes on non-suspense

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,17 @@
+import { lazy } from "preact-iso";
 import { sideBar } from "../content/data";
 import BaseLayout from "../layouts/base";
+import { Suspense } from "preact/compat";
 
 const AllContent = () =>
   Object.keys(sideBar).map((d, i) => {
-    const Component = sideBar[d]?.source || (() => <></>);
+    const Component = sideBar[d].source;
     const classList = i > 0 ? "mt-12 pt-12" : "";
-    return <Component class={classList} />;
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Component class={classList} />
+      </Suspense>
+    );
   });
 
 export default function HomePage() {
